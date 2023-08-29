b69f1bc4debf9a09d5e1f19844c5d3b84f4b1bb1
Isaac Turner <spacey_sooty@outlook.com>
2023-08-29 10:20:57 +0800

    fixed method issues

--- lib/src/pid/mod.rs
+++ lib/src/pid/mod.rs
@@ -13,16 +13,27 @@ pub struct PIDConfig {
 
 impl PIDConfig {
   // getters for fields
-  fn get_path(&self) -> &String { &self.path }
-  fn get_p(&self) -> f64 { self.p }
-  fn get_i(&self) -> f64 { self.i }
-  fn get_d(&self) -> f64 { self.d }
-  fn get_izone(&self) -> f64 { self.izone }
-  fn get_error(&self) -> f64 { self.error }
+  pub fn get_path(&self) -> &String { &self.path }
+  pub fn get_p(&self) -> f64 { self.p }
+  pub fn get_i(&self) -> f64 { self.i }
+  pub fn get_d(&self) -> f64 { self.d }
+  pub fn get_izone(&self) -> f64 { self.izone }
+  pub fn get_error(&self) -> f64 { self.error }
 
   // setters for mutable values
-  fn set_izone(&mut self, val: f64) { self.izone = val }
-  fn set_error(&mut self, val: f64) { self.error = val }
+  pub fn set_izone(&mut self, val: f64) { self.izone = val }
+  pub fn set_error(&mut self, val: f64) { self.error = val }
+
+  pub fn from(path: String, p: f64, i: f64, d: f64, izone: f64, error: f64) -> PIDConfig {
+    return PIDConfig {
+        path: path,
+        p: p,
+        i: i,
+        d: d,
+        izone: izone,
+        error: error
+    };
+  }
 }
 
 pub enum System {
@@ -46,23 +57,23 @@ pub struct PIDController {
 
 impl PIDController {
   // getters
-  fn get_config(&self) -> &PIDConfig { &self.config }
-  fn get_system(&self) -> &System { &self.system }
+  pub fn get_config(&self) -> &PIDConfig { &self.config }
+  pub fn get_system(&self) -> &System { &self.system }
 
   // closed loop
-  fn closed_loop_c(&mut self) -> f64 {
+  pub fn closed_loop_c(&mut self) -> f64 {
       let x = self.config.p + self.config.i / self.dt + self.config.d * self.dt;
       self.c = x;
       return x; 
   }
 
-  fn on_update(&mut self) {
+  pub fn on_update(&mut self) {
       self.closed_loop_c();
       if self.config.izone > self.target - self.c {
           self.config.d -= 0.1;
       }
 
-      if (-self.config.error < self.target - self.c && self.target - self.c < self.config.error) {
+      if -self.config.error < self.target - self.c && self.target - self.c < self.config.error {
         self.finished = true;
       }
   }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate ev3dev_lang_rust;
 
 use ev3dev_lang_rust::Ev3Result;
 use ev3dev_lang_rust::motors::{LargeMotor, MotorPort};
-use ev3dev_lang_rust::sensors::ColorSensor;
 
 fn main() -> Ev3Result<()> {
 
@@ -14,15 +13,6 @@ fn main() -> Ev3Result<()> {
 
     // Run motor.
     large_motor.set_duty_cycle_sp(50)?;
-
-    // Find color sensor. Always returns the first recognized one.
-    let color_sensor = ColorSensor::find()?;
-
-    // Switch to rgb mode.
-    color_sensor.set_mode_rgb_raw()?;
-
-    // Get current rgb color tuple.
-    println!("Current rgb color: {:?}", color_sensor.get_rgb()?);
 
     Ok(())
 }
