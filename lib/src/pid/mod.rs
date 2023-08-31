b3d916343fd9006ba66b20a5b85ed4a5032ba1bc
Isaac Turner <spacey_sooty@outlook.com>
2023-08-31 09:11:54 +0800

    fixed speed reduction as the loop approaches its target

--- lib/src/pid/mod.rs
+++ lib/src/pid/mod.rs
@@ -3,28 +3,28 @@ use ev3dev_lang_rust::motors::{LargeMotor, MediumMotor};
 pub struct PIDConfig {
   // immutable
   path: String,
-  p: f64,
-  i: f64,
-  d: f64,
+  p: f32,
+  i: f32,
+  d: f32,
   // mutable
-  izone: f64,
-  error: f64,
+  izone: f32,
+  error: f32,
 }
 
 impl PIDConfig {
   // getters for fields
   pub fn get_path(&self) -> &String { &self.path }
-  pub fn get_p(&self) -> f64 { self.p }
-  pub fn get_i(&self) -> f64 { self.i }
-  pub fn get_d(&self) -> f64 { self.d }
-  pub fn get_izone(&self) -> f64 { self.izone }
-  pub fn get_error(&self) -> f64 { self.error }
+  pub fn get_p(&self) -> f32 { self.p }
+  pub fn get_i(&self) -> f32 { self.i }
+  pub fn get_d(&self) -> f32 { self.d }
+  pub fn get_izone(&self) -> f32 { self.izone }
+  pub fn get_error(&self) -> f32 { self.error }
 
   // setters for mutable values
-  pub fn set_izone(&mut self, val: f64) { self.izone = val }
-  pub fn set_error(&mut self, val: f64) { self.error = val }
+  pub fn set_izone(&mut self, val: f32) { self.izone = val }
+  pub fn set_error(&mut self, val: f32) { self.error = val }
 
-  pub fn from(path: String, p: f64, i: f64, d: f64, izone: f64, error: f64) -> PIDConfig {
+  pub fn from(path: String, p: f32, i: f32, d: f32, izone: f32, error: f32) -> PIDConfig {
     return PIDConfig {
         path: path,
         p: p,
@@ -46,12 +46,12 @@ impl System {}
 pub struct PIDController {
   // mutable (for values that are mutable in PIDConfig type)
   config: PIDConfig,
-  target: f64,
-  dt: f64,
+  target: f32,
+  dt: f32,
   // immutable
   // the system that is controlled by this PID controller instance
   system: System,
-  c: f64,
+  c: f32,
   finished: bool
 }
 
@@ -61,16 +61,18 @@ impl PIDController {
   pub fn get_system(&self) -> &System { &self.system }
 
   // closed loop
-  pub fn closed_loop_c(&mut self) -> f64 {
-      let x = self.config.p + self.config.i / self.dt + self.config.d * self.dt;
-      self.c = x;
-      return x; 
+  pub fn closed_loop_c(&mut self) {
+      self.c = self.config.p + self.config.i / self.dt + self.config.d * self.dt;
   }
 
   pub fn on_update(&mut self) {
       self.closed_loop_c();
+      match &self.system {
+          System::LargeMotor(motor) => motor.set_duty_cycle_sp(50 + self.config.d as i32).unwrap(),
+          System::MediumMotor(motor) => motor.set_duty_cycle_sp(50 + self.config.d as i32).unwrap(),
+      };
       if self.config.izone > self.target - self.c {
-          self.config.d -= 0.1;
+          self.config.d -= 0.35;
       }
 
       if -self.config.error < self.target - self.c && self.target - self.c < self.config.error {
